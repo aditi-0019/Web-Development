cc32931e51de757a6b62070a2c4ea414463fc19e
Aditi <singhaditi.as75@gmail.com>
2026-07-09 22:49:43 +0530

    data types

---
+++ Javascriptfiles/basics/datatypes.js
@@ -0,0 +1,14 @@
+//  **Data types** 
+// number: 2 to the power 53
+// string:assembly of characters 
+// bigint:for large numbers 
+//   boolean:true/false;
+//   null:standalone value (type=object) means its empty 
+//   undefined:kinda/placeholder for a value which is not defind yet (type=undefined ) 
+//   symbol:uniqueness
+
+
+
+console.log(typeof 45);  //number 
+console.log(typeof undefined); //undefined 
+console.log(typeof null);  //object
