267483d889e199f75bce353f214696c53233f339
Aditi Singh <singhaditi.as75@gmail.com>
2026-09-07 21:57:04 +0530

    basics

---
+++ Javascriptfiles/basics/2nd.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all the JS code as newer version
+
+// alert(3+3)  //we are using nodejs not browser so it will throw an error 
+
+console.log(3+3) ;
+ console.log("Aditi") 
+
+ let name="Aditi"
+ let age=18
+ let isloggedIn=false 
+
+
+ 
+ /*
+ 6
+Aditi
+ */ 
