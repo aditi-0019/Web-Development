d00242190b5dd57bdde1807003de06f026c56e91
Aditi Singh <singhaditi.as75@gmail.com>
2026-09-07 21:57:57 +0530

    boolean and operations

---
+++ Javascriptfiles/basics/bool.js
@@ -0,0 +1,50 @@
+  /*let isLoggedIn=1 // true
+ let isLoggedIn="" //false 
+let isLoggedIn= "Aditi" //true
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);   */
+ 
+
+// Conversion into string
+let someNum= 33 
+let stringNumber = String(someNum) ;
+console.log(stringNumber);  //33 
+console.log(typeof stringNumber); //string 
+
+  // ******OPERATIONS******
+
+  let value=3
+  let negValue=-value
+  console.log(negValue) //-3
+  console.log(2+2)  //4
+  console.log(2-2)  //0
+  console.log(3**2) //9
+  console.log(2/3)  //0.6666
+  console.log(2%3)  //2
+
+  //  OPERATIONS ON STRING 
+   let str1="Hello"
+   let str2="Aditi"
+   let str3=str1+str2
+   console.log(str3);  //HelloAditi
+
+   console.log("1"+2); //12
+   console.log(1+"2"); //12
+   console.log("1"+2+2); //122
+   console.log(1 +2+"2"); //32  ECMA Script rules 
+   console.log((3+4)+5%3)   //9
+   console.log(true) //true 
+   console.log(+true) //1 here its  converstion and then increment is required but here only the conversion takes place
+  //  console.log(true+) //Error 
+  console.log(+"");  //0 here only the conversion takes place
+ 
+
+  let num1,num2,num3
+  num1=num2=num3= 2+2
+  console.log(num2);  //4
+  let gameCounter =100
+  gameCounter++;
+  console.log(gameCounter); //101
+  //  ++gameCounter
+  // console.log(gameCounter); //101
+
